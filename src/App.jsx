5427cffea29fae1ea726cfe73fe8effe072c9248
tf <107359683@umail.ucc.ie>
2024-03-09 12:42:25 +0000

    added environment map for lighting

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import { Controllers, VRButton, XR } from "@react-three/xr";
 import { Canvas } from "@react-three/fiber";
 import World from "./World/World";
 import { Perf } from "r3f-perf";
+import { Environment } from "@react-three/drei";
 
 function App() {
   return (
     <>
       <VRButton />
       <Canvas camera={{ fov: 65, near: 0.1, far: 300 }}>
+        <Environment preset="city" />
         <XR>
-          <ambientLight intensity={2} />
+          <ambientLight />
           <Perf position="top-left" />
           <Controllers />
           <World />
