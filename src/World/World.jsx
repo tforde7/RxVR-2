0dfafcbe229ad85f9abd7e462d67d8f26fcce198
tf <107359683@umail.ucc.ie>
2024-03-10 11:00:12 +0000

    debugging teleportation plane

--- src/World/World.jsx
+++ src/World/World.jsx
@@ -1,16 +1,9 @@
 import { OrbitControls, Sky } from "@react-three/drei";
-import { TeleportationPlane, useInteraction, useXR } from "@react-three/xr";
+import { TeleportationPlane, useXR } from "@react-three/xr";
 import { useControls } from "leva";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import Outside from "./Outside/Outside";
 import { Physics } from "@react-three/rapier";
-import LectureTheatre from "./LectureTheatre/LectureTheatre";
-import Lobby from "./Lobby/Lobby";
-import { BuildingsCombined } from "./BuildingsCombined";
-import { Beatrizz } from "./Tour/Beatrizz";
-import { MRITV } from "./MRI/MRITV";
-import { TestTV } from "./TestTV";
-import { XRayTV } from "./XRay/XRayTV";
 
 const World = () => {
   const { cameraTarget, orbitControlsEnabled } = useControls({
@@ -18,46 +11,18 @@ const World = () => {
       value: { x: 0, y: 0, z: 0 },
       step: 0.1,
     },
-    orbitControlsEnabled: false,
-  });
-
-  const { playerPosition, playerRotation } = useControls("Player", {
-    playerPosition: {
-      value: {
-        x: 15,
-        y: 1.6,
-        z: 0,
-      },
-      step: 0.1,
-    },
-    playerRotation: {
-      value: {
-        x: 0,
-        y: 0,
-        z: 0,
-      },
-      step: 0.01,
-    },
+    orbitControlsEnabled: true,
   });
 
   const { isPresenting, player } = useXR();
 
   useEffect(() => {
     if (!isPresenting) {
-      player.position.set(playerPosition.x, 1.6, 0);
+      player.position.set(0, 1.6, 0);
     } else {
-      player.position.set(playerPosition.x, 0, 0);
+      player.position.set(0, 0, 0);
     }
   }, [isPresenting]);
-
-  const teleportationPlane = useRef();
-
-  const teleportSound = new Audio("/sounds/sfx/whoosh.mp3");
-
-  useInteraction(teleportationPlane, "onSqueeze", () => {
-    teleportSound.play();
-  });
-
   return (
     <>
       <Sky />
@@ -67,19 +32,108 @@ const World = () => {
       />
       <Physics>
         <Outside />
-        <LectureTheatre />
-        <Lobby />
-        <BuildingsCombined />
-        <Beatrizz />
-        <MRITV />
-        <XRayTV />
-        {/* <TestTV /> */}
+        {/* <BuildingsTemplate /> */}
+        {/* <LectureTheatre /> */}
+        {/* <Lobby />
+        <MainConcourse /> */}
+        {/* <LobbyAndMainConcourse /> */}
+        {/* <LobbyAndMainConcourse4 /> */}
+        {/* <SeahorseContainer /> */}
+        {/* <MRIRoom /> */}
+        {/* <XRayRoom /> */}
+        {/* <XrayDepartment /> */}
+        {/* <MRIDepartment /> */}
+        {/* <Hoppy scale={0.5} position-z={-3} /> */}
       </Physics>
-      {isPresenting && (
-        <TeleportationPlane ref={teleportationPlane} rightHand />
-      )}
+      {isPresenting && <TeleportationPlane rightHand />}
     </>
   );
 };
 
 export default World;
+
+// import { OrbitControls, Sky } from "@react-three/drei";
+// import { TeleportationPlane, useInteraction, useXR } from "@react-three/xr";
+// import { useControls } from "leva";
+// import { useEffect, useRef } from "react";
+// import Outside from "./Outside/Outside";
+// import { Physics } from "@react-three/rapier";
+// import LectureTheatre from "./LectureTheatre/LectureTheatre";
+// import Lobby from "./Lobby/Lobby";
+// import { BuildingsCombined } from "./BuildingsCombined";
+// import { Beatrizz } from "./Tour/Beatrizz";
+// import { MRITV } from "./MRI/MRITV";
+// import { TestTV } from "./TestTV";
+// import { XRayTV } from "./XRay/XRayTV";
+
+// const World = () => {
+//   const { cameraTarget, orbitControlsEnabled } = useControls({
+//     cameraTarget: {
+//       value: { x: 0, y: 0, z: 0 },
+//       step: 0.1,
+//     },
+//     orbitControlsEnabled: false,
+//   });
+
+//   const { playerPosition, playerRotation } = useControls("Player", {
+//     playerPosition: {
+//       value: {
+//         x: 15,
+//         y: 1.6,
+//         z: 0,
+//       },
+//       step: 0.1,
+//     },
+//     playerRotation: {
+//       value: {
+//         x: 0,
+//         y: 0,
+//         z: 0,
+//       },
+//       step: 0.01,
+//     },
+//   });
+
+//   const { isPresenting, player } = useXR();
+
+//   useEffect(() => {
+//     if (!isPresenting) {
+//       player.position.set(playerPosition.x, 1.6, 0);
+//     } else {
+//       player.position.set(playerPosition.x, 0, 0);
+//     }
+//   }, [isPresenting]);
+
+//   const teleportationPlane = useRef();
+
+//   const teleportSound = new Audio("/sounds/sfx/whoosh.mp3");
+
+//   useInteraction(teleportationPlane, "onSqueeze", () => {
+//     teleportSound.play();
+//   });
+
+//   return (
+//     <>
+//       <Sky />
+//       <OrbitControls
+//         enabled={orbitControlsEnabled}
+//         target={[cameraTarget.x, cameraTarget.y, cameraTarget.z]}
+//       />
+//       <Physics>
+//         <Outside />
+//         <LectureTheatre />
+//         <Lobby />
+//         <BuildingsCombined />
+//         <Beatrizz />
+//         <MRITV />
+//         <XRayTV />
+//         {/* <TestTV /> */}
+//       </Physics>
+//       {isPresenting && (
+//         <TeleportationPlane ref={teleportationPlane} rightHand />
+//       )}
+//     </>
+//   );
+// };
+
+// export default World;
