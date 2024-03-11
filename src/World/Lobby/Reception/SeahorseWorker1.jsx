a3613bb400f153e3ae874a6edd541a3981dd7cd0
tf <107359683@umail.ucc.ie>
2024-03-11 20:56:33 +0000

    added dialogues, interactions for seahorse worker 1

--- src/World/Lobby/Reception/SeahorseWorker1.jsx
+++ src/World/Lobby/Reception/SeahorseWorker1.jsx
@@ -1,56 +1,79 @@
-import { useAnimations, useGLTF } from "@react-three/drei";
+import { Sparkles, useAnimations, useGLTF } from "@react-three/drei";
 import { RigidBody } from "@react-three/rapier";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRef } from "react";
-import { useInteraction } from "@react-three/xr";
+import { useInteraction, useXR } from "@react-three/xr";
 
 export default function Worker1() {
   const worker = useGLTF("/models/npcs/Low Poly Women/Woman in Tank Top.glb");
+  console.log(worker);
 
-  //     function findAnimations(obj) {
-  //       if (obj.animations && obj.animations.length > 0) {
-  //         return obj.animations;
-  //       } else {
-  //         if (obj.children) {
-  //           return obj.children.forEach(findAnimations);
-  //         }
-  //       }
-  //     }
+  const workerRef = useRef();
 
-  //   Call findAnimations on the root of the object tree
-  //     const animationClips = findAnimations(worker);
+  const [isHovered, setIsHovered] = useState(false); // Track hover state
+  const [dialoguePlaying, setDialoguePlaying] = useState(false); // Track dialogue state
 
-  //     const animations = useAnimations(animationClips, worker.scene);
-  //     if (animations.actions) {
-  //       animations.actions["Take 001"].play();
-  //     }
+  // Hover interaction handler
+
+  const interactionSound = new Audio("/sounds/sfx/pop.mp3");
+  interactionSound.volume = 0.5;
+
+  const handleHover = (hovering) => {
+    setIsHovered(hovering);
+    if (hovering) {
+      interactionSound.play();
+    }
+  };
+
+  useInteraction(workerRef, "onHover", () => handleHover(true));
+  useInteraction(workerRef, "onBlur", () => handleHover(false));
+
+  const { actions } = useAnimations(worker.animations, workerRef);
+
+  const ACTION_PREFIX = "HumanArmature|";
+
+  const idleAnimation = actions[ACTION_PREFIX + "Female_Idle"];
+
+  useEffect(() => {
+    if (idleAnimation) {
+      idleAnimation.play();
+    }
+  }, [idleAnimation]);
+
+  const audio = new Audio("/sounds/seahorse-nurse/seahorse-nurse.mp3");
+
+  const { player } = useXR();
+
+  useInteraction(workerRef, "onSelect", (interactionEvent) => {
+    if (interactionEvent.target.inputSource.handedness === "right") return;
+    if (dialoguePlaying) return;
+
+    workerRef.current.lookAt(player.children[0].position);
+
+    audio.play();
+    setDialoguePlaying(true);
+
+    audio.onended = () => {
+      setDialoguePlaying(false);
+    };
+  });
 
   // Define fixed values for scale, position, and rotation
   const scale = [0.3, 0.3, 0.3]; // Example scale
   const position = [53, 0, -60]; // Example position
   const rotation = [0, 0.88, 0]; // Example rotation (in radians)
 
-  //   const workerHello = new Audio("/sounds/doctor/doctor-hello.mp3");
-
-  //   const workerRef = useRef();
-
-  //   useInteraction(workerRef, "onSelect", (event) => {
-  //     if (event.target.inputSource.handedness === "right") {
-  //       return;
-  //     }
-  //     workerHello.play();
-  //   });
-
   return (
     <>
       <RigidBody colliders="hull" type="fixed">
         <primitive
-          //ref={workerRef}
+          ref={workerRef}
           object={worker.scene}
           scale={scale}
           position={position}
           rotation={rotation}
         />
+        {isHovered && <Sparkles color={"yellow"} />}
       </RigidBody>
     </>
   );
